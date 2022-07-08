8882e4f92b3a81811b937b52b86f2d8d5cd50338
Neftalí Toledo <47830827+neftalito@users.noreply.github.com>
2022-07-08 16:08:25 -0300

    Añadir solucion con operador ternario en el 2.2.11

--- Soluciones/2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/11.js
+++ Soluciones/2. Programación Imperativa/2. Práctica Funciones y Tipos de Datos/11.js
@@ -6,3 +6,11 @@ function valorEnvido(nCarta){
         return nCarta
     }
 }
+/*
+Utilizando operadores ternarios no se puede resolver este ejercicio porque la pagina no lo permite
+Pero se puede resolver de la proxima manera
+function valorEnvido(nCarta){
+    return nCarta >= 10 && nCarta <= 12 ? 0 : nCarta
+}
+
+*/
